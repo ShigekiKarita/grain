4b3c983efa4a99b6ada4c5ec8beb48b5258c5ca4
karita <shigekikarita@gmail.com>
2018-06-09 02:19:35 +0900

    add .travis.yml

---
+++ source/grain/package.d
@@ -0,0 +1,21 @@
+/++
+
+Chainer like autograd and neural networks library
+
+ +/
+
+
+
+/**
+   License: $(LINK2 http://boost.org/LICENSE_1_0.txt, Boost License 1.0).
+   Authors:
+   $(LINK2 https://github.com/ShigekiKarita, Shigeki Karita),
+
+   See_Also:
+   $(LINK2 https://github.com/ShigekiKarita/grain, Github repo)
+   $(BR)
+   $(LINK2 https://chainer.org, chainer)
+   $(BR)
+   $(LINK2 https://pytorch.org, pytorch)
+   $(BR)
+*/
