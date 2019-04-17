d0d20584ebb0de685f4cfc9d109fa1ab169fe51f
karita <shigekikarita@gmail.com>
2019-04-17 23:21:27 +0900

    update circle ci

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-sudo apt-get install libopenblas-dev libzmq5-dev cmake
+sudo apt-get install libopenblas-dev libzmq3-dev libhdf5-dev cmake
 
 source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
 dub test --build=unittest-cov
